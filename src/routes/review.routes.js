f76495e2fc4e750e8e25c29d7bcd9e066dd8cd6c
abdulaz1z31 <a.abdulaziz3110@gmail.com>
2024-11-18 20:22:08 +0500

    task completed first version

--- src/routes/review.routes.js
+++ src/routes/review.routes.js
@@ -6,7 +6,7 @@ import {
     updateReviewById,
 } from '../controllers/index.js'
 import { validationReviews } from '../validation/review.validation.js'
-import { authGuard } from '../middlewares/index.js'
+import { authGuard, roleGuard } from '../middlewares/index.js'
 
 export const reviewRouter = Router()
 
@@ -14,4 +14,9 @@ export const reviewRouter = Router()
 reviewRouter.post('/review', authGuard, validationReviews, createReview)
 reviewRouter.get('/review/:id', authGuard, getReviewById)
 reviewRouter.put('/review/:id', authGuard, validationReviews, updateReviewById)
-reviewRouter.delete('/review/:id', authGuard, deleteReviewById)
+reviewRouter.delete(
+    '/review/:id',
+    authGuard,
+    roleGuard('admin'),
+    deleteReviewById,
+)
