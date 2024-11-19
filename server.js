d41ad99b730f19da0bed1447fc727d817d19b698
Asadbek Zaynobiddinov <asadbekzaynobiddinov51@gmail.com>
2024-11-19 14:18:35 +0500

    Added

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import { logger } from './src/utils/logger.js'
 const bootstrap = async () => {
     try {
         app.listen(config.app.port, () => {
-            logger.info(`server running on port: ${config.app.port}.`)
+            logger.info(`server running on port: ${config.app.port}..`)
         })
     } catch (error) {
         logger.error(error)
